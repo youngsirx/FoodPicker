7ee98edb0f8ebbd48189430571f45d0522ada3ae
Jordan <Jordan@ADVP-JYOUNG>
2016-05-05 14:03:28 -0300

    changes to the details pages and index of food and restaurant

---
+++ FoodPicker/ViewModels/FoodIndexData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FoodPicker.Models;
+
+namespace FoodPicker.ViewModels
+{
+    public class FoodIndexData
+    {
+        public IEnumerable<Food> Foods { get; set; }
+    }
+}
