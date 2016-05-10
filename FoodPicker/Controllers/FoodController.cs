88429849777a93ceb0972a26b8bc5ca4e591d6ea
Jordan <Jordan@ADVP-JYOUNG>
2016-05-10 15:40:44 -0300

    updated food controller and food view and added helpers for image upload

--- FoodPicker/Controllers/FoodController.cs
+++ FoodPicker/Controllers/FoodController.cs
@@ -8,6 +8,8 @@ using System.Web;
 using System.Web.Mvc;
 using FoodPicker.DAL;
 using FoodPicker.Models;
+using System.Threading.Tasks;
+using FoodPicker.Helpers;
 
 namespace FoodPicker.Controllers
 {
@@ -49,16 +51,64 @@ namespace FoodPicker.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "FoodID,Name,Description,ImageName,RestaurantID,Price,DateAdded,AverageRating")] Food food)
+        public async Task<ActionResult> Create([Bind(Include = "FoodID,Name,Description,RestaurantID,Price")] Food food, HttpPostedFileBase ImageName)
         {
+
+            ViewBag.ResturantID = new SelectList(db.Restaurants, "RestaurantID", "Name", food.RestaurantID);
+
+            //jyoung: added image upload
+            //added HttpPostedFileBase ImageName args in method
+            //Also the form needs an enctype="multipart/form-data"
             if (ModelState.IsValid)
             {
-                db.Foods.Add(food);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+                //check if you have anything to upload
+                if (ImageName != null && ImageName.ContentLength > 0)
+                {
+                    
+                    var validImageTypes = new string[]
+                   {
+                        //"image/gif",
+                        //"image/jpg",
+                        //"image/jpeg",
+                        "image/png"
+                   };
+                    if (!validImageTypes.Contains(ImageName.ContentType))
+                    {
+                        //file being uploaded is not a png -display error
+                        ModelState.AddModelError("", "Please use a PNG image only.");
+                        return View(food);
+                    }
 
-            ViewBag.RestaurantID = new SelectList(db.Restaurants, "RestaurantID", "Name", food.RestaurantID);
+                    food.DateAdded = DateTime.Today;
+                    
+                    
+                    //save new food to database
+                    db.Foods.Add(food);
+                    await db.SaveChangesAsync();
+
+                    //retrieve the IDENTITY (new name for image) FROM sql sERVER
+                    string pictureName = food.FoodID.ToString();
+
+
+                    //next rename, scale an upload the image.
+                    ImageUpload imageUpload = new ImageUpload { Width = 200, Height = 275 };
+                    ImageResult imageResult = imageUpload.RenameUploadFile(ImageName, pictureName);
+
+                    food.ImageName = pictureName + ".png";
+                    db.Entry(food).State = EntityState.Modified;
+                    await db.SaveChangesAsync();
+
+                    return RedirectToAction("Details", new { id = food.FoodID});
+                }
+                else
+                {
+                    ModelState.AddModelError("", "You have not selected an image file to upload.");
+                    return View(food);
+                }
+               
+            }//end of modelstate
+
+            
             return View(food);
         }
 
@@ -83,17 +133,101 @@ namespace FoodPicker.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "FoodID,Name,Description,ImageName,RestaurantID,Price,DateAdded,AverageRating")] Food food)
+        public async Task<ActionResult> Edit([Bind(Include = "Name,Description,Price")] Food food, HttpPostedFileBase ImageName)
         {
+
+            ViewBag.RestaurantID = new SelectList(db.Restaurants, "RestaurantID", "Name", food.RestaurantID);
             if (ModelState.IsValid)
             {
-                db.Entry(food).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                //check if you have anything to upload
+                if (ImageName != null && ImageName.ContentLength > 0)
+                {
+
+                    var validImageTypes = new string[]
+                   {
+                        //"image/gif",
+                        //"image/jpg",
+                        //"image/jpeg",
+                        "image/png"
+                   };
+                    if (!validImageTypes.Contains(ImageName.ContentType))
+                    {
+                        //file being uploaded is not a png -display error
+                        ModelState.AddModelError("", "Please use a PNG image only.");
+                        return View(food);
+                    }
+                                       
+                    //save new food to database
+                    db.Foods.Add(food);
+                    await db.SaveChangesAsync();
+
+                    //retrieve the IDENTITY (new name for image) FROM sql sERVER
+                    string pictureName = food.ImageName.ToString();
+                    
+                    //next rename, scale an upload the image.
+                    ImageUpload imageUpload = new ImageUpload { Width = 200, Height = 275 };
+                    ImageResult imageResult = imageUpload.RenameUploadFile(ImageName, pictureName);
+
+                    
+                    return RedirectToAction("Details", new { id = food.FoodID });
+                }
+              
+  
             }
-            ViewBag.RestaurantID = new SelectList(db.Restaurants, "RestaurantID", "Name", food.RestaurantID);
+          
             return View(food);
         }
+        //public async Task<ActionResult> EditPost(int? id, HttpPostedFileBase ImageName)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+
+        //    var foodToUpdate = db.Foods.Find(id);
+        //    if (TryUpdateModel(foodToUpdate, "",
+        //        new string[] { "Name", "Description", "Price" }))
+        //    {
+
+        //        if (ImageName != null && ImageName.ContentLength > 0)
+        //        {
+        //            //do we have anything to upload - yes
+        //            var validImageTypes = new string[]
+        //            {
+        //                //"image/gif",
+        //                //"image/jpg",
+        //                //"image/jpeg",
+        //                "image/png"
+        //            };
+        //            if (!validImageTypes.Contains(ImageName.ContentType))
+        //            {
+        //                //file being uploaded is not a png -display error
+        //                ModelState.AddModelError("", "Please choose a PNG image.");
+        //                return View(foodToUpdate);
+
+        //            }
+
+        //            try
+        //            {
+        //                await db.SaveChangesAsync();
+        
+        //                string pictureName = foodToUpdate.ToString();
+
+        //                //next rename, scale an upload the image.
+        //                ImageUpload imageUpload = new ImageUpload { Width = 200, Height = 275 };
+        //                ImageResult imageResult = imageUpload.RenameUploadFile(ImageName, pictureName);
+
+        //                return RedirectToAction("Details", new {id = foodToUpdate });
+        //            }
+        //          
+        //        }
+
+        //    }
+        //    return View("Details", new { id = foodToUpdate });
+        //}
+
+
+
 
         // GET: Food/Delete/5
         public ActionResult Delete(int? id)
@@ -115,7 +249,13 @@ namespace FoodPicker.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
+            
+
             Food food = db.Foods.Find(id);
+
+            string image = food.ImageName.ToString();
+           
+
             db.Foods.Remove(food);
             db.SaveChanges();
             return RedirectToAction("Index");
